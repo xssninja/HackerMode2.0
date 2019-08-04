1030025149640c5e5bdc0583e57b1caf95e99b15
David Cross <superdave.cross@gmail.com>
2019-08-03 23:06:00 -0600

    Updating Kali_Read.php
    Adding more protections to Kali_Read.php but you still should set no-sniff HTTP headers etc.

--- PHP Site/Kali_Read.php
+++ PHP Site/Kali_Read.php
@@ -1,8 +1,18 @@
 <?php
 
 // return the command from the text file to keep the file 
-$file = file_get_contents('4KaliQueue.txt', FILE_USE_INCLUDE_PATH);
-echo $file;
+$cmd = file_get_contents('4KaliQueue___.txt', FILE_USE_INCLUDE_PATH);
+// new remove suspect characters
+$cmd = str_replace("<","",$cmd);
+//$cmd = str_replace("data:","data.",$cmd);
+$cmd = str_replace("?>","",$cmd);
+$cmd = str_replace("(","",$cmd);
+$cmd = str_replace(")","",$cmd);
+$cmd = str_replace("//","",$cmd);
+$cmd = str_replace("HackerModeQueueData:","",$cmd);
+
+echo $cmd;
+// You may need to change _DIR_ to 'https://example.com'
 $response = file_get_contents(__DIR__ . '/4KaliClear.php');
 
 ?>
