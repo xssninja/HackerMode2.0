879cac7bf41d81d5cd76cf802b8cd64294de2d6a
David Cross <superdave.cross@gmail.com>
2019-08-03 23:27:08 -0600

    New config file that needs compilation before run
    This config needs to have the {your domain here} replaced with the domain of your PHP hosting site. The ports should be OK to leave as is. Change them only if you absolutely know what you're doing.
    COMPILE THIS before you run Alexapwn6.py because it will break otherwise.
    python -m py compile alexapwn6cfg.py
    That will compile it to a .pyc then it can be properly used by the Alexapwn6.py program.

---
+++ Kali/alexapwn6cfg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+kalicfg = {'Kali2Alexa': 'https://{your domain here}/Kali2Alexa.php',
+         'Kali_Read': 'https://{your domain here}/Kali_Read.php',
+         'default_lport': '4000',
+		 'default_msfrpc_port': 55553}
