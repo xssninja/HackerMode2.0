7bcebcd3f27529324310ec93bcae64294d91717c
David Cross <superdave.cross@gmail.com>
2019-08-03 22:58:26 -0600

    Updated Alexa_Read.php
    With added protection for payload contents. Still need your HTTP headers!

--- PHP Site/Alexa_Read.php
+++ PHP Site/Alexa_Read.php
@@ -1,10 +1,16 @@
 <?php
-
 // return the command from the text file to keep the file 
-$file = file_get_contents('4AlexaQueue.txt', FILE_USE_INCLUDE_PATH);
-echo $file;
-// clear the queue file out
+$cmd = file_get_contents('4AlexaQueue.txt', FILE_USE_INCLUDE_PATH);
+// new remove suspect characters
+$cmd = str_replace("<","",$cmd);
+$cmd = str_replace("?>","",$cmd);
+$cmd = str_replace("(","",$cmd);
+$cmd = str_replace(")","",$cmd);
+$cmd = str_replace("//","",$cmd);
+$cmd = str_replace("HackerModeQueueData:","",$cmd);
 
+echo $cmd;
+// clear the queue file out... you may need to replace _DIR_ with your domain like this:
+//$response = file_get_contents('https://example.com/4AlexaClear.php');
 $response = file_get_contents(__DIR__ .'/4AlexaClear.php');
-
 ?>
